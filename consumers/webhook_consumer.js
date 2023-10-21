2a1d860bf2c86e73d20040b851dfed02ae1a202b
Dat Nguyen <kenzouno1@gmail.com>
2023-10-21 11:10:20 +0700

    remove dotenv

--- consumers/webhook_consumer.js
+++ consumers/webhook_consumer.js
@@ -8,8 +8,8 @@ export class WebhookConsumer {
   send = async (announcement) => {
     
     let body = {
-      "id": "oc_aa83061161511928e455c27065d9d94a",
-      "templateId": "ctp_AAmY9fBDM3m1",
+      "id": process.env.CHANEL_ID,
+      "templateId": process.env.TEMPLATE_ID,
       params: {
         title: announcement.platform +" Update Api",
         content: announcement.title,
